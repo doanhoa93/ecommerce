32fb128cacc97678e96bea50410c8c819a1118db
Nguyen Huu Tien <tiennh1995@gmail.com>
2018-02-09 21:00:04 +0700

    admin list orders

--- src/main/java/com/framgia/controller/admin/PagesController.java
+++ src/main/java/com/framgia/controller/admin/PagesController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller("admin")
+@Controller("admin/page")
 @RequestMapping(value = "admin")
 public class PagesController extends AdminController {
 
